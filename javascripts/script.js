105497ebd6843d61d6923ecc741828d6a8ea0bec
Sumer Mixon <gitSumer@ximm.us>
2014-04-24 18:01:49 -0400

    color selector

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,3 +1,9 @@
+var color = "color-000000";
+
+$( ".colorwheel li" ).click( function () {
+  color = $( this ).attr('class');
+});
+
 $( ".pixel" ).click( function () {
-  $( this ).toggleClass( "painted" );
+  $( this ).toggleClass( color );
 });
