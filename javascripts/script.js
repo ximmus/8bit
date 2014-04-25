e43cbbad8d95e6a059555108ddfa6cb1bd59cfda
Sumer Mixon <sumer@ximm.us>
2014-04-24 21:15:14 -0400

    color repaint function complete

--- javascripts/script.js
+++ javascripts/script.js
@@ -5,5 +5,20 @@ $( ".colorwheel li" ).click( function () {
 });
 
 $( ".pixel" ).click( function () {
-  $( this ).toggleClass( color );
+  
+  // var current = $( this ).attr( "class" );
+
+  // $( this ).removeClass( current );
+  // $( this ).addClass( "pixel" );
+
+	// $( this ).attr( "class",
+ //  		function( pos, classes ){
+ //    	return classes.replace(/\bcolor-\S+/g, "");
+	// });
+
+	$( this ).removeClass (function (index, css) {
+    	return (css.match (/\bcolor-\S+/g) || []).join(' ');
+	});
+
+	$( this ).addClass( color );
 });
